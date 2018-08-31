5b852a690b042e7187feb89090cc6d162f9e4048
classicalliu <classicalliu@gmail.com>
2018-08-31 16:20:20 +0800

    add staging and production deploy script

---
+++ config/deploy/production.rb
@@ -0,0 +1,4 @@
+set :domain, '47.96.73.126'
+set :deploy_to, '/home/cita/nervos-cita-server'
+set :branch, 'master'
+set :user, 'cita'
---
+++ config/deploy/staging.rb
@@ -0,0 +1,4 @@
+set :domain, '47.99.40.201'
+set :deploy_to, '/home/cita/nervos-cita-server'
+set :branch, 'release'
+set :user, 'cita'
